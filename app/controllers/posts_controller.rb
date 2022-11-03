e80755ca77c03e1545ece366847bfb168c0b9194
aaaaayumu <112301397+aaaaayumu@users.noreply.github.com>
2022-11-03 19:11:59 +0900

    Basic認証

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :basic_auth
 
   def index
     @posts = Post.order(id: "DESC")
@@ -10,5 +11,12 @@ class PostsController < ApplicationController
   def create
     post = Post.create(content: params[:content])
     render json: { post:post }
+  end
+
+  private
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
   end
 end
